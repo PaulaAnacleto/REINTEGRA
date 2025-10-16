c76e177a4e2b0aa78d30a6f270e30e201cf3bbaf
Anna Luísa <annaluisateixeira8@gmail.com>
2025-10-15 21:16:42 -0300

    feat: Adição dos arquivos para a página de informativos.

--- View/Informativos.php
+++ View/Informativos.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Informativos</title>
+     <link rel="stylesheet" href="../Templates/css/informativos.css">
 </head>
 <body>
-    
+     <div class="container">
+        <!-- Header -->
+        <header class="header">
+            <button class="back-button" aria-label="Voltar">
+                <img src="../Img/página de contato/seta.png" alt="Voltar" class="back-icon">
+            </button>
+        
 </body>
 </html>
