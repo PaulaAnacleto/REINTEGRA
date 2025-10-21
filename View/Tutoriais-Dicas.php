0910e2d2ca990ed6220d102afd4c835d86e84cca
Cordeiro17 <gui99295l@gmail.com>
2025-10-21 10:24:07 -0300

    feature/início

--- View/Tutoriais-Dicas.php
+++ View/Tutoriais-Dicas.php
@@ -5,59 +5,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tutoriais e Dicas - Reintegra</title>
 
-     <!-- Bootstrap CSS -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Google Fonts -->
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
-    rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="../Templates/css/Tutoriais-dicas.css">
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container">
-        <!-- <a class="navbar-brand" href="#"> -->
-        <div class="navbar-left">
-            <div class="logo-box">
-                <span class="logo-text">REINTEGRA</span>
-            </div>
+    <!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <div class="logo-box">
+                    <span class="logo-text">REINTEGRA</span>
+                </div>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
-        </div>
-        <div class="navbar-right">
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#serviços">Serviços</a>
+                        <a class="nav-link" href="../view/Servicos">Serviços</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#contato">Feedback</a>
+                        <a class="nav-link" href="#cadastro">Feedback</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#contato">Contato</a>
+                        <a class="nav-link" href="../View/Contato.php">Contato</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#sobre">Sobre</a>
                     </li>
-                    <li   class="nav-item">
-                     <a class="nav-link" href="#inicio">Início</a> 
+                    <li class="nav-item">
+                        <a class="nav-link" href="../View/Inicial-login.php">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="../View/Perfil.php">
+                        <button class="btn-perfil" onclick="abrirPerfil()">
+                            <img src="../Img/página de perfil/icone perfil.png" class="img-perfil" alt="Perfil">
+                        </button>
+                        </a>
                     </li>
                 </ul>
             </div>
-            <li class="nav-item-img" style="list-style-type: none;">
-                <img src="../Img/página de perfil/icone perfil.png" class="img-perfil" alt="Perfil">
-            </li>
         </div>
-    </div>
-</nav>
+    </nav>
 
-    <div class="container">
+
         <section class="hero-section">
         <button class="back-button" aria-label="Voltar">
                 <img src="../Img/página de contato/seta.png" alt="Voltar" class="back-icon">
