c2da6fc2fb2d399aead0e64a79517ea6c250e59b
PaulaAnacleto <paulaanacleto15@gmail.com>
2025-10-22 10:34:57 -0300

    fix: finalizado

---
+++ Templates/js/curriculo.js
@@ -0,0 +1,13 @@
+    const backButton = document.querySelector('.back-button');
+    
+    if (backButton) {
+        backButton.addEventListener('click', function() {
+            // Verifica se há histórico de navegação
+            if (window.history.length > 1) {
+                window.history.back();
+            } else {
+                // Se não houver histórico, redireciona para a página inicial
+                window.location.href = '../View/inicial-login.php';
+            }
+        });
+    }
