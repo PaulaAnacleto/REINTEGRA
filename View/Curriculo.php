1c00bf3a3447c5a46c8ecc387676118257fa4df6
Cordeiro17 <gui99295l@gmail.com>
2025-10-23 11:59:46 -0300

    Adição da api Vlibras

--- View/Curriculo.php
+++ View/Curriculo.php
@@ -17,6 +17,19 @@
 </head>
 <body>
 
+
+    <div vw class="enabled">
+  <div vw-access-button class="active"></div>
+  <div vw-plugin-wrapper>
+    <div class="vw-plugin-top-wrapper"></div>
+  </div>
+</div>
+<script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+<script>
+  new window.VLibras.Widget('https://vlibras.gov.br/app' );
+</script>
+
+
     <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
   <div class="container">
