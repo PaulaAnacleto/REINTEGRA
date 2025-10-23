34e013aa060dda4699097e56f00d75316147e0aa
Anna Luísa <annaluisateixeira8@gmail.com>
2025-10-23 12:01:41 -0300

    feat: Adição do JS.

--- View/Cursos.php
+++ View/Cursos.php
@@ -18,44 +18,45 @@
     <link rel="stylesheet" href="../Templates/css/cursos.css">
 </head>
 
-<nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container">
-        <!-- <a class="navbar-brand" href="#"> -->
-        <div class="navbar-left">
-            <div class="logo-box">
-                <span class="logo-text">REINTEGRA</span>
-            </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <div class="logo-box">
+                    <span class="logo-text">REINTEGRA</span>
+                </div>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
-        </div>
-        <div class="navbar-right">
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#serviços">Serviços</a>
+                        <a class="nav-link" href="../view/Servicos">Serviços</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#contato">Feedback</a>
+                        <a class="nav-link" href="../View/Inicial-login.php">Feedback</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#contato">Contato</a>
+                        <a class="nav-link" href="../View/Contato.php">Contato</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#sobre">Sobre</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#inicio">Início</a>
+                        <a class="nav-link" href="../View/Inicial-login.php">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="../View/Perfil.php">
+                        <button class="btn-perfil" onclick="abrirPerfil()">
+                            <img src="../Img/página de perfil/icone perfil.png" class="img-perfil" alt="Perfil">
+                        </button>
+                        </a>
                     </li>
                 </ul>
             </div>
-            <li class="nav-item-img" style="list-style-type: none;">
-                <img src="../Img/página de perfil/icone perfil.png" class="img-perfil" alt="Perfil">
-            </li>
         </div>
-    </div>
-</nav>
+    </nav>
+
 <!-- Header -->
 <header class="header">
     <div class="btn-retorno">
