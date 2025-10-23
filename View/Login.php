9e2d673a70877ef176b342884d93ce4bbe57508d
Cordeiro17 <gui99295l@gmail.com>
2025-10-23 11:39:54 -0300

    Adição da api Vlibras

--- View/Login.php
+++ View/Login.php
@@ -6,7 +6,20 @@
     <title>Login - REINTEGRA</title>
     <link rel="stylesheet" href="../Templates/css/login.css">
 </head>
-<body>
+<body>]
+
+
+    <div vw class="enabled">
+  <div vw-access-button class="active"></div>
+  <div vw-plugin-wrapper>
+    <div class="vw-plugin-top-wrapper"></div>
+  </div>
+</div>
+<script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+<script>
+  new window.VLibras.Widget('https://vlibras.gov.br/app' );
+</script>
+
     <div class="container">
         <!-- Seção esquerda com branding -->
         <div class="left-section">
