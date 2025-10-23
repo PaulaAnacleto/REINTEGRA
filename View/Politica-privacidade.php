bf48e2181d49035577af813f7a538de15871f5ef
Cordeiro17 <gui99295l@gmail.com>
2025-10-23 11:46:04 -0300

    Adição da api Vlibras

--- View/Politica-privacidade.php
+++ View/Politica-privacidade.php
@@ -7,6 +7,19 @@
     <link rel="stylesheet" href="../Templates/css/politica-privacidade.css">
 </head>
 <body>
+
+
+    <div vw class="enabled">
+  <div vw-access-button class="active"></div>
+  <div vw-plugin-wrapper>
+    <div class="vw-plugin-top-wrapper"></div>
+  </div>
+</div>
+<script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+<script>
+  new window.VLibras.Widget('https://vlibras.gov.br/app' );
+</script>
+
     <div class="container">
         <!-- Header Section -->
         <header class="header">
