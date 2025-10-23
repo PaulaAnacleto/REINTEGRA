3096eeb99a3e99df7b7310ac1b0676ff3abaed02
Cordeiro17 <gui99295l@gmail.com>
2025-10-23 12:03:58 -0300

    Adição da api Vlibras

--- View/Automacao-Robotica.php
+++ View/Automacao-Robotica.php
@@ -67,6 +67,19 @@
 
 
 <body>
+
+
+    <div vw class="enabled">
+  <div vw-access-button class="active"></div>
+  <div vw-plugin-wrapper>
+    <div class="vw-plugin-top-wrapper"></div>
+  </div>
+</div>
+<script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+<script>
+  new window.VLibras.Widget('https://vlibras.gov.br/app' );
+</script>
+
     <div class="inicio">
         <div class="inicio-inner">
             <div class="texto-inicio">
