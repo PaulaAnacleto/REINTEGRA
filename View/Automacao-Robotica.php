766b40dfd0e75ae17f0550b09674f10062ce2dda
Cordeiro17 <gui99295l@gmail.com>
2025-10-28 07:47:27 -0300

    edição do Vlibras

--- View/Automacao-Robotica.php
+++ View/Automacao-Robotica.php
@@ -67,9 +67,7 @@
 
 
 <body>
-
-
-
+    
     <div vw class="enabled">
   <div vw-access-button class="active"></div>
   <div vw-plugin-wrapper>
@@ -79,7 +77,8 @@
 <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
 <script>
   new window.VLibras.Widget('https://vlibras.gov.br/app' );
-</script>
+</script>]
+
 
     <div class="inicio">
         <div class="inicio-inner">
