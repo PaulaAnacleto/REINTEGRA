a05a8c354a5e7160317462347aab4744e7040b2a
PaulaAnacleto <paulaanacleto15@gmail.com>
2025-10-29 08:24:53 -0300

    back-end

--- Config/configuration.php
+++ Config/configuration.php
@@ -2,7 +2,7 @@
 // Define as constantes de conexão com o banco
 define('DB_HOST', 'localhost');      // Ou 127.0.0.1
 define('DB_USER', 'root');           // Seu usuário do MySQL
-define('DB_PASS', 'sua_senha');    // <-- MUDE AQUI para sua senha
+define('DB_PASS', '');    // <-- MUDE AQUI para sua senha
 define('DB_NAME', 'reintegra_db'); // O nome do seu banco
 define('DB_CHARSET', 'utf8mb4');
 ?>
